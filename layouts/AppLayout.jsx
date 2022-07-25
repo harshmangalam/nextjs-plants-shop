03b47f439638c2c556b3c1e24fdfd71557740d50
harshmangalam <harshdev8218@gmail.com>
2022-07-25 22:12:01 +0530

    added app bar

--- layouts/AppLayout.jsx
+++ layouts/AppLayout.jsx
@@ -1,17 +1,37 @@
-import { Box } from "@mui/material";
-
-export default function AppLayout({children}){
-
-    return (
-        <Box>
-            {/* navbar  */}
-                navbar
-
-            {/* main  */}
-                {children}
-
-            {/* footer  */}
-            footer
-        </Box>
-    )
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+export default function AppLayout({ children }) {
+  return (
+    <Box>
+      {/* navbar  start */}
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Plants
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      {/* navbar end  */}
+      {/* main  */}
+      {children}
+      {/* footer  */}
+      footer
+    </Box>
+  );
 }
