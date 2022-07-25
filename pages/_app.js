4e799929d105501520acf57d6ed30ad10d25e50f
harshmangalam <harshdev8218@gmail.com>
2022-07-25 21:46:30 +0530

    setup mui

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,23 @@
+import React from 'react';
+import { CacheProvider } from '@emotion/react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import createEmotionCache from '../utility/createEmotionCache';
+import lightTheme from '../themes/lightTheme';
 
-export default MyApp
+const clientSideEmotionCache = createEmotionCache();
+
+const MyApp = (props) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+};
+
+export default MyApp;
