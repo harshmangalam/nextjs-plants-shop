edb60b21bb49c318667052928ea568a2b192d317
harshmangalam <harshdev8218@gmail.com>
2022-07-26 18:58:45 +0530

    added links

--- layouts/AppLayout.jsx
+++ layouts/AppLayout.jsx
@@ -19,23 +19,39 @@ export default function AppLayout({ children }) {
       {/* navbar  start */}
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Plants
-          </Typography>
-          <Stack direction="row" spacing={2}>
-            <Link passHref href={"/cart"}>
-              <IconButton
-                size="small"
-                edge="start"
-                color="inherit"
-                aria-label="cart"
+          <Stack
+            direction="row"
+            justifyContent={"space-between"}
+            width="100%"
+            alignItems={"center"}
+          >
+            <Link href="/" passHref>
+              <Stack
+                sx={{ cursor: "pointer" }}
+                direction="row"
+                spacing={1}
+                alignItems="center"
               >
-                <Badge badgeContent={4} color="secondary">
-                  <ShoppingCartIcon />
-                </Badge>
-              </IconButton>
+                <YardIcon fontSize="large" />
+                <Typography variant="h6">Plants</Typography>
+              </Stack>
             </Link>
-            <AccountMenu />
+
+            <Stack direction="row" spacing={2}>
+              <Link passHref href={"/cart"}>
+                <IconButton
+                  size="small"
+                  edge="start"
+                  color="inherit"
+                  aria-label="cart"
+                >
+                  <Badge badgeContent={4} color="secondary">
+                    <ShoppingCartIcon />
+                  </Badge>
+                </IconButton>
+              </Link>
+              <AccountMenu />
+            </Stack>
           </Stack>
         </Toolbar>
       </AppBar>
