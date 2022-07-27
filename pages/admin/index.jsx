017eea84c601282d6fa412065167a66884fa98e0
harshmangalam <harshdev8218@gmail.com>
2022-07-27 09:52:26 +0530

    added admin dashboard

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -1,7 +1,30 @@
+import { Avatar, Grid, Paper, Stack, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import AdminLayout from "../../layouts/AdminLayout"
+import AdminLayout from "../../layouts/AdminLayout";
+import PeopleAltOutlinedIcon from "@mui/icons-material/PeopleAltOutlined";
 export default function AdminHome() {
-  return <Box>Admin</Box>;
+  return (
+    <Box>
+      {/* countings */}
+      <Grid container spacing={4}>
+        {[...new Array(4)].map((item) => (
+          <Grid item md={3}>
+            <Paper sx={{ padding: 2 }}>
+              <Stack direction={"row"} spacing={2}>
+                <Avatar sx={{ bgcolor: (theme) => theme.palette.success }}>
+                  <PeopleAltOutlinedIcon />
+                </Avatar>
+                <Stack>
+                  <Typography variant="body1">Users</Typography>
+                  <Typography variant="h6">459</Typography>
+                </Stack>
+              </Stack>
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
 }
 
 AdminHome.getLayout = function getLayout(page) {
