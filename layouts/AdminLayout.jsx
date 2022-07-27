5685ba8c4055b09c40e142dd431c08796cbdf578
harshmangalam <harshdev8218@gmail.com>
2022-07-27 09:32:16 +0530

    added admin drawer

--- layouts/AdminLayout.jsx
+++ layouts/AdminLayout.jsx
@@ -1,9 +1,88 @@
-import { Box } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Drawer,
+  IconButton,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
 
-export default function AdminLayout({children}){
-    return (
-        <Box>
-            {children}
-        </Box>
-    )
+// import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
+
+
+const drawerWidth = 300;
+export default function AdminLayout({ children }) {
+  const [openDrawer, setOpenDrawer] = useState(false);
+  return (
+    <Box>
+      {/* appbar  start */}
+
+      <AppBar
+        position="fixed"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <Toolbar>
+          <Tooltip title={openDrawer ? "Close" : "Open"}>
+            <IconButton
+              color="inherit"
+              aria-label="toggle drawer"
+              onClick={() => setOpenDrawer((o) => !o)}
+              edge="start"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Tooltip>
+          <Typography variant="h6" noWrap component="div">
+            Clipped drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      {/* appbar end  */}
+
+      {/* drawer start  */}
+
+      <Drawer
+        variant="persistent"
+        anchor="left"
+        open={openDrawer}
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+      >
+        <Toolbar />
+        {/* <List>
+          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List> */}
+      </Drawer>
+      {/* drawer end  */}
+      {children}
+    </Box>
+  );
 }
+
+
+// const menus =[
+//     {
+//         name:"Dashboard",
+//         href:"/admin",
+//         icon:
+//     }
+// ]
