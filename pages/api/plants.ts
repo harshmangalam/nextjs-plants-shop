bf5060d29c46633629c0373a529ed2671b779b2b
harshmangalam <harshdev8218@gmail.com>
2022-08-05 11:29:00 +0530

    added create plant api

---
+++ pages/api/plants.ts
@@ -0,0 +1,52 @@
+/**
+ * Plant api
+ * edit
+ * delete
+ * create
+ *
+ */
+
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../lib/prisma";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    // get plant data from body 
+    const { name, images, description, price, categoryId } = JSON.parse(
+      req.body
+    );
+    try {
+        // create new plant 
+      const newPlant = await prisma.plant.create({
+        data: {
+          name,
+          images,
+          description,
+          price: Number(price),
+          categoryId,
+        },
+        select: {
+          id: true,
+          quantity: true,
+        },
+      });
+
+    //   incr the quantity by 1
+      await prisma.plant.update({
+        where: {
+          id: newPlant.id,
+        },
+        data: {
+          quantity: newPlant.quantity + 1,
+        },
+      });
+
+      return res.status(200).json({ message: "Plant created successfully" });
+    } catch (error) {
+      return res.status(500).json({ error: "Error while creating plant" });
+    }
+  }
+}
