6d35b9c06a9aa41c7424ddb72c24683afb0e915f
harshmangalam <harshdev8218@gmail.com>
2022-08-06 12:56:42 +0530

    added delete plant api

--- pages/api/plants.ts
+++ pages/api/plants.ts
@@ -78,4 +78,28 @@ export default async function handler(
       return res.status(500).json({ error: "Error while updating plant" });
     }
   }
+
+  if (req.method === "DELETE") {
+    try {
+      const { plantId } = req.body;
+      const plant = await prisma.plant.findUnique({
+        where: {
+          id: plantId,
+        },
+      });
+
+      if (!plant) {
+        return res.status(404).json({ error: "Plant not found" });
+      }
+      await prisma.plant.delete({
+        where: {
+          id: plantId,
+        },
+      });
+      return res.status(200).json({ message: "Plant deleted successfully" });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error: "Error while deleting plant" });
+    }
+  }
 }
