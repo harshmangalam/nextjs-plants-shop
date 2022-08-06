729fde91fa2903f198a8971779cdec6182c91e7e
harshmangalam <harshdev8218@gmail.com>
2022-08-06 12:53:21 +0530

    added edit plant api

--- pages/api/plants.ts
+++ pages/api/plants.ts
@@ -14,12 +14,12 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
-    // get plant data from body 
+    // get plant data from body
     const { name, images, description, price, categoryId } = JSON.parse(
       req.body
     );
     try {
-        // create new plant 
+      // create new plant
       const newPlant = await prisma.plant.create({
         data: {
           name,
@@ -34,7 +34,7 @@ export default async function handler(
         },
       });
 
-    //   incr the quantity by 1
+      //   incr the quantity by 1
       await prisma.plant.update({
         where: {
           id: newPlant.id,
@@ -47,6 +47,35 @@ export default async function handler(
       return res.status(200).json({ message: "Plant created successfully" });
     } catch (error) {
       return res.status(500).json({ error: "Error while creating plant" });
+    }
+  }
+
+  if (req.method === "PUT") {
+    // get plant data from body
+    const { id, name, images, description, price, categoryId } = JSON.parse(
+      req.body
+    );
+    try {
+      // create new plant
+      await prisma.plant.update({
+        where: {
+          id,
+        },
+        data: {
+          name,
+          images,
+          description,
+          price: Number(price),
+          categoryId,
+        },
+        select: {
+          id: true,
+        },
+      });
+
+      return res.status(200).json({ message: "Plant updated successfully" });
+    } catch (error) {
+      return res.status(500).json({ error: "Error while updating plant" });
     }
   }
 }
