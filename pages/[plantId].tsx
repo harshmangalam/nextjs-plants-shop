6c8cf3b9466827952578a5acad31cbf174165508
harshmangalam <harshdev8218@gmail.com>
2022-08-07 13:46:57 +0530

    added plant details

--- pages/[plantId].tsx
+++ pages/[plantId].tsx
@@ -12,9 +12,10 @@ import { Box } from "@mui/system";
 import StarIcon from "@mui/icons-material/Star";
 import { useState } from "react";
 import CurrencyRupeeIcon from "@mui/icons-material/CurrencyRupee";
-import AppLayout from "../layouts/AppLayout"
+import AppLayout from "../layouts/AppLayout";
+import prisma from "../lib/prisma";
 export default function PlantDetails({ plant }) {
-  const [previewImage, setPreviewImage] = useState(plant.images[0]);
+  const [previewImage, setPreviewImage] = useState(plant.images[0].url);
 
   return (
     <Box>
@@ -30,11 +31,11 @@ export default function PlantDetails({ plant }) {
             <Stack direction={"row"} spacing={2}>
               {plant.images.map((image) => (
                 <Card>
-                  <CardActionArea onClick={() => setPreviewImage(image)}>
+                  <CardActionArea onClick={() => setPreviewImage(image.url)}>
                     <CardMedia
                       sx={{ height: 100, width: 100, objectFit: "cover" }}
                       component={"img"}
-                      image={image}
+                      image={image.url}
                     />
                   </CardActionArea>
                 </Card>
@@ -48,22 +49,20 @@ export default function PlantDetails({ plant }) {
 
             <Stack direction={"row"} spacing={1} alignItems="center">
               <CurrencyRupeeIcon fontSize="medium" />
-              <Typography variant="h6">399</Typography>
+              <Typography variant="h6">{plant.price}</Typography>
             </Stack>
 
             <Stack direction="row" spacing={1} alignItems="center">
               <Rating
                 name="plant-feedbacks"
-                value={plant.review.avgRating}
+                value={4.1}
                 readOnly
                 precision={0.5}
                 emptyIcon={
                   <StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />
                 }
               />
-              <Typography variant="subtitle1">
-                {plant.review.total} reviews
-              </Typography>
+              <Typography variant="subtitle1">400 reviews</Typography>
             </Stack>
 
             <Typography paragraph>{plant.description}</Typography>
@@ -81,27 +80,14 @@ export default function PlantDetails({ plant }) {
 
 export async function getServerSideProps({ params }) {
   const { plantId } = params;
+  const plant = await prisma.plant.findUnique({
+    where: {
+      id: plantId,
+    },
+  });
   return {
     props: {
-      plant: {
-        _id: plantId,
-        name: "Peace Lily, Spathiphyllum - Plant",
-        description: `Peace Lily Plant is a very popular and very rare indoor flowering houseplant. It is also an excellent air purifier plant.Peace Lily Plant is a very popular and very rare indoor flowering houseplant. It is also an excellent air purifier plant.Peace Lily Plant is a very popular and very rare indoor flowering houseplant. It is also an excellent air purifier plant.Peace Lily Plant is a very popular and very rare indoor flowering houseplant. It is also an excellent air purifier plant`,
-        review: {
-          total: 56,
-          avgRating: 4.5,
-        },
-        price: 346,
-
-        images: [
-          "https://cdn.shopify.com/s/files/1/0047/9730/0847/products/nurserylive-g-plants-peace-lily-spathiphyllum-plant_600x600.jpg?v=1637123920",
-          "https://cdn.shopify.com/s/files/1/0047/9730/0847/products/nurserylive-plants-peace-lily-spathiphyllum-plant-1_540x720.jpg?v=1637123918",
-          "https://cdn.shopify.com/s/files/1/0047/9730/0847/products/nurserylive-plants-peace-lily-spathiphyllum-plant-2_540x720.jpg?v=1637123917",
-          "https://cdn.shopify.com/s/files/1/0047/9730/0847/products/nurserylive-plants-peace-lily-spathiphyllum-plant-3_540x720.jpg?v=1637123917",
-          "https://cdn.shopify.com/s/files/1/0047/9730/0847/products/nurserylive-plants-peace-lily-spathiphyllum-plant-4_540x720.jpg?v=1637123921",
-          "https://cdn.shopify.com/s/files/1/0047/9730/0847/products/nurserylive-plants-peace-lily-spathiphyllum-plant-5_540x720.jpg?v=1637123917",
-        ],
-      },
+      plant: JSON.parse(JSON.stringify(plant)),
     },
   };
 }
